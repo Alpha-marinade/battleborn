2802a1155d0c7b764a49ade8f0ccec2f7c56d34c
alpha_marinade <alphamarinade@gmail.com>
2025-01-31 20:51:35 +0300

    lighning rod render fix

--- src/main/java/org/battleborn/battleborn/Battleborn.java
+++ src/main/java/org/battleborn/battleborn/Battleborn.java
@@ -68,19 +68,21 @@ public class Battleborn {
         modEventBus.addListener(this::addCreative);
 
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
+        /*
         if (Config.logDirtBlock) LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
 
         LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
 
         Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+
+         */
     }
 
     // Add the example block item to the building blocks tab
